2c79ad886b520630e43259476a54314d25871bcd
zhiwe.niu <zhiwei.niu@advancegroup.com>
2023-04-04 14:13:01 +0800

    添加工具类

---
+++ shepherd-server/src/main/java/org/shepherd/server/Application.java
@@ -0,0 +1,26 @@
+package org.shepherd.server;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author niuzhiwei
+ */
+@Slf4j
+@SpringBootApplication(scanBasePackages = {"com.*"})
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Application.class);
+        app.run(args);
+        System.out.println(
+                "   _____ _    _  _____ _____ ______  _____ _____ \n" +
+                        "  / ____| |  | |/ ____/ ____|  ____|/ ____/ ____|\n" +
+                        " | (___ | |  | | |   | |    | |__  | (___| (___  \n" +
+                        "  \\___ \\| |  | | |   | |    |  __|  \\___ \\\\___ \\ \n" +
+                        "  ____) | |__| | |___| |____| |____ ____) |___) |\n" +
+                        " |_____/ \\____/ \\_____\\_____|______|_____/_____/ \n");
+    }
+
+}
