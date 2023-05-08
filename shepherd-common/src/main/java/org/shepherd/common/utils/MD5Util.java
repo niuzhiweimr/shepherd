b4072b708cd1ce29d1c845ab2f33ecc6ecc009cd
zhiwe.niu <zhiwei.niu@advancegroup.com>
2023-05-08 17:25:32 +0800

    添加枚举

--- shepherd-common/src/main/java/org/shepherd/common/utils/MD5Util.java
+++ shepherd-common/src/main/java/org/shepherd/common/utils/MD5Util.java
@@ -5,7 +5,6 @@ import java.security.MessageDigest;
 import static org.apache.commons.lang3.StringUtils.isEmpty;
 
 /**
- *
  * @author niuzhiwei
  * @date 2019/3/8
  */
@@ -26,11 +25,11 @@ public class MD5Util {
             MessageDigest md = MessageDigest.getInstance("MD5");
             md.update((source + mid5key).getBytes(encoder));
             byte[] hash = md.digest();
-            for (int i = 0; i < hash.length; i++) {
-                if ((0xff & hash[i]) < 0x10) {
-                    hexString.append("0" + Integer.toHexString((0xFF & hash[i])));
+            for (byte b : hash) {
+                if ((0xff & b) < 0x10) {
+                    hexString.append("0").append(Integer.toHexString((0xFF & b)));
                 } else {
-                    hexString.append(Integer.toHexString(0xFF & hash[i]));
+                    hexString.append(Integer.toHexString(0xFF & b));
                 }
             }
         }
